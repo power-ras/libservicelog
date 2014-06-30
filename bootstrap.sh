1f24800fc827ca8126cb2cbc191533c9fa819192
Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
2014-06-30 13:43:07 +0530

    Cleanup configure and remove NEWS file
    Removed unused macros from configure.ac file, added code
    to create config and m4 directory.
    
    Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,14 @@
+set -e
+
+if [ ! -d config ];
+then
+	mkdir config;
+fi
+
+if [ ! -d m4 ];
+then
+	mkdir m4;
+fi
+
 aclocal && libtoolize --force && autoheader && \
 automake --add-missing --copy && autoconf
