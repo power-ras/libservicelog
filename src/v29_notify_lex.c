e76215f2012dde21a17c3e83c94c2720bd58d46b
Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>
2015-03-23 10:09:35 +0530

    v29_notify_lex.c: Cleanup leading/trailing space
    Cleanup leading/trailing spaces.
    
    Signed-off-by: Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>
    Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>

--- src/v29_notify_lex.c
+++ src/v29_notify_lex.c
@@ -53,7 +53,7 @@
 #if defined (__STDC_VERSION__) && __STDC_VERSION__ >= 199901L
 
 /* C99 says to define __STDC_LIMIT_MACROS before including stdint.h,
- * if you want the limit (max/min) macros for int types. 
+ * if you want the limit (max/min) macros for int types.
  */
 #ifndef __STDC_LIMIT_MACROS
 #define __STDC_LIMIT_MACROS 1
@@ -70,7 +70,7 @@ typedef uint32_t flex_uint32_t;
 typedef signed char flex_int8_t;
 typedef short int flex_int16_t;
 typedef int flex_int32_t;
-typedef unsigned char flex_uint8_t; 
+typedef unsigned char flex_uint8_t;
 typedef unsigned short int flex_uint16_t;
 typedef unsigned int flex_uint32_t;
 #endif /* ! C99 */
@@ -181,7 +181,7 @@ extern FILE *v29nfy_in, *v29nfy_out;
 #define EOB_ACT_LAST_MATCH 2
 
     #define YY_LESS_LINENO(n)
-    
+
 /* Return all but the first "n" matched characters back to the input stream. */
 #define yyless(n) \
 	do \
@@ -243,7 +243,7 @@ struct yy_buffer_state
 
     int yy_bs_lineno; /**< The line count. */
     int yy_bs_column; /**< The column count. */
-    
+
 	/* Whether to try to fill the input buffer when we reach the
 	 * end of it.
 	 */
@@ -561,7 +561,7 @@ char *v29nfy_text;
 
 static int yy_init_globals (void );
 
-/* Accessor methods to globals.
+/* Accessors methods to globals.
    These are made visible to non-reentrant scanners for convenience. */
 
 int v29nfy_lex_destroy (void );
@@ -603,7 +603,7 @@ extern int v29nfy_wrap (void );
 #endif
 
     static void yyunput (int c,char *buf_ptr  );
-    
+
 #ifndef yytext_ptr
 static void yy_flex_strncpy (char *,yyconst char *,int );
 #endif
@@ -724,7 +724,7 @@ YY_DECL
 	register yy_state_type yy_current_state;
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
-    
+
 #line 15 "v29_notify_lex.l"
 
 
@@ -1091,7 +1091,7 @@ case YY_STATE_EOF(INITIAL):
  */
 static int yy_get_next_buffer (void)
 {
-    	register char *dest = YY_CURRENT_BUFFER_LVALUE->yy_ch_buf;
+	register char *dest = YY_CURRENT_BUFFER_LVALUE->yy_ch_buf;
 	register char *source = (yytext_ptr);
 	register int number_to_move, i;
 	int ret_val;
@@ -1227,7 +1227,7 @@ static int yy_get_next_buffer (void)
 {
 	register yy_state_type yy_current_state;
 	register char *yy_cp;
-    
+
 	yy_current_state = (yy_start);
 
 	for ( yy_cp = (yytext_ptr) + YY_MORE_ADJ; yy_cp < (yy_c_buf_p); ++yy_cp )
@@ -1258,7 +1258,7 @@ static int yy_get_next_buffer (void)
     static yy_state_type yy_try_NUL_trans  (yy_state_type yy_current_state )
 {
 	register int yy_is_jam;
-    	register char *yy_cp = (yy_c_buf_p);
+	register char *yy_cp = (yy_c_buf_p);
 
 	register YY_CHAR yy_c = 1;
 	if ( yy_accept[yy_current_state] )
@@ -1281,7 +1281,7 @@ static int yy_get_next_buffer (void)
     static void yyunput (int c, register char * yy_bp )
 {
 	register char *yy_cp;
-    
+
     yy_cp = (yy_c_buf_p);
 
 	/* undo effects of setting up v29nfy_text */
@@ -1324,7 +1324,7 @@ static int yy_get_next_buffer (void)
 
 {
 	int c;
-    
+
 	*(yy_c_buf_p) = (yy_hold_char);
 
 	if ( *(yy_c_buf_p) == YY_END_OF_BUFFER_CHAR )
@@ -1391,12 +1391,12 @@ static int yy_get_next_buffer (void)
 
 /** Immediately switch to a different input stream.
  * @param input_file A readable stream.
- * 
+ *
  * @note This function does not reset the start condition to @c INITIAL .
  */
     void v29nfy_restart  (FILE * input_file )
 {
-    
+
 	if ( ! YY_CURRENT_BUFFER ){
         v29nfy_ensure_buffer_stack ();
 		YY_CURRENT_BUFFER_LVALUE =
@@ -1409,11 +1409,11 @@ static int yy_get_next_buffer (void)
 
 /** Switch to a different input buffer.
  * @param new_buffer The new input buffer.
- * 
+ *
  */
     void v29nfy__switch_to_buffer  (YY_BUFFER_STATE  new_buffer )
 {
-    
+
 	/* TODO. We should be able to replace this entire function body
 	 * with
 	 *		v29nfy_pop_buffer_state();
@@ -1444,7 +1444,7 @@ static int yy_get_next_buffer (void)
 
 static void v29nfy__load_buffer_state  (void)
 {
-    	(yy_n_chars) = YY_CURRENT_BUFFER_LVALUE->yy_n_chars;
+	(yy_n_chars) = YY_CURRENT_BUFFER_LVALUE->yy_n_chars;
 	(yytext_ptr) = (yy_c_buf_p) = YY_CURRENT_BUFFER_LVALUE->yy_buf_pos;
 	v29nfy_in = YY_CURRENT_BUFFER_LVALUE->yy_input_file;
 	(yy_hold_char) = *(yy_c_buf_p);
@@ -1453,13 +1453,13 @@ static void v29nfy__load_buffer_state  (void)
 /** Allocate and initialize an input buffer state.
  * @param file A readable stream.
  * @param size The character buffer size in bytes. When in doubt, use @c YY_BUF_SIZE.
- * 
+ *
  * @return the allocated buffer state.
  */
     YY_BUFFER_STATE v29nfy__create_buffer  (FILE * file, int  size )
 {
 	YY_BUFFER_STATE b;
-    
+
 	b = (YY_BUFFER_STATE) v29nfy_alloc(sizeof( struct yy_buffer_state )  );
 	if ( ! b )
 		YY_FATAL_ERROR( "out of dynamic memory in v29nfy__create_buffer()" );
@@ -1482,11 +1482,11 @@ static void v29nfy__load_buffer_state  (void)
 
 /** Destroy the buffer.
  * @param b a buffer created with v29nfy__create_buffer()
- * 
+ *
  */
     void v29nfy__delete_buffer (YY_BUFFER_STATE  b )
 {
-    
+
 	if ( ! b )
 		return;
 
@@ -1502,7 +1502,7 @@ static void v29nfy__load_buffer_state  (void)
 #ifndef __cplusplus
 extern int isatty (int );
 #endif /* __cplusplus */
-    
+
 /* Initializes or reinitializes a buffer.
  * This function is sometimes called more than once on the same buffer,
  * such as during a v29nfy_restart() or at EOF.
@@ -1511,7 +1511,7 @@ extern int isatty (int );
 
 {
 	int oerrno = errno;
-    
+
 	v29nfy__flush_buffer(b );
 
 	b->yy_input_file = file;
@@ -1527,17 +1527,17 @@ extern int isatty (int );
     }
 
         b->yy_is_interactive = file ? (isatty( fileno(file) ) > 0) : 0;
-    
+
 	errno = oerrno;
 }
 
 /** Discard all buffered characters. On the next scan, YY_INPUT will be called.
  * @param b the buffer state to be flushed, usually @c YY_CURRENT_BUFFER.
- * 
+ *
  */
     void v29nfy__flush_buffer (YY_BUFFER_STATE  b )
 {
-    	if ( ! b )
+	if ( ! b )
 		return;
 
 	b->yy_n_chars = 0;
@@ -1562,11 +1562,11 @@ extern int isatty (int );
  *  the current state. This function will allocate the stack
  *  if necessary.
  *  @param new_buffer The new state.
- *  
+ *
  */
 void v29nfy_push_buffer_state (YY_BUFFER_STATE new_buffer )
 {
-    	if (new_buffer == NULL)
+	if (new_buffer == NULL)
 		return;
 
 	v29nfy_ensure_buffer_stack();
@@ -1592,11 +1592,11 @@ void v29nfy_push_buffer_state (YY_BUFFER_STATE new_buffer )
 
 /** Removes and deletes the top of the stack, if present.
  *  The next element becomes the new top.
- *  
+ *
  */
 void v29nfy_pop_buffer_state (void)
 {
-    	if (!YY_CURRENT_BUFFER)
+	if (!YY_CURRENT_BUFFER)
 		return;
 
 	v29nfy__delete_buffer(YY_CURRENT_BUFFER );
@@ -1616,7 +1616,7 @@ void v29nfy_pop_buffer_state (void)
 static void v29nfy_ensure_buffer_stack (void)
 {
 	int num_to_alloc;
-    
+
 	if (!(yy_buffer_stack)) {
 
 		/* First allocation is just for 2 elements, since we don't know if this
@@ -1629,9 +1629,9 @@ static void v29nfy_ensure_buffer_stack (void)
 								);
 		if ( ! (yy_buffer_stack) )
 			YY_FATAL_ERROR( "out of dynamic memory in v29nfy_ensure_buffer_stack()" );
-								  
+
 		memset((yy_buffer_stack), 0, num_to_alloc * sizeof(struct yy_buffer_state*));
-				
+
 		(yy_buffer_stack_max) = num_to_alloc;
 		(yy_buffer_stack_top) = 0;
 		return;
@@ -1659,13 +1659,13 @@ static void v29nfy_ensure_buffer_stack (void)
 /** Setup the input buffer state to scan directly from a user-specified character buffer.
  * @param base the character buffer
  * @param size the size in bytes of the character buffer
- * 
- * @return the newly allocated buffer state object. 
+ *
+ * @return the newly allocated buffer state object.
  */
 YY_BUFFER_STATE v29nfy__scan_buffer  (char * base, yy_size_t  size )
 {
 	YY_BUFFER_STATE b;
-    
+
 	if ( size < 2 ||
 	     base[size-2] != YY_END_OF_BUFFER_CHAR ||
 	     base[size-1] != YY_END_OF_BUFFER_CHAR )
@@ -1694,14 +1694,14 @@ YY_BUFFER_STATE v29nfy__scan_buffer  (char * base, yy_size_t  size )
 /** Setup the input buffer state to scan a string. The next call to v29nfy_lex() will
  * scan from a @e copy of @a str.
  * @param yystr a NUL-terminated string to scan
- * 
+ *
  * @return the newly allocated buffer state object.
  * @note If you want to scan bytes that may contain NUL values, then use
  *       v29nfy__scan_bytes() instead.
  */
 YY_BUFFER_STATE v29nfy__scan_string (yyconst char * yystr )
 {
-    
+
 	return v29nfy__scan_bytes(yystr,strlen(yystr) );
 }
 
@@ -1709,7 +1709,7 @@ YY_BUFFER_STATE v29nfy__scan_string (yyconst char * yystr )
  * scan from a @e copy of @a bytes.
  * @param bytes the byte buffer to scan
  * @param len the number of bytes in the buffer pointed to by @a bytes.
- * 
+ *
  * @return the newly allocated buffer state object.
  */
 YY_BUFFER_STATE v29nfy__scan_bytes  (yyconst char * yybytes, int  _yybytes_len )
@@ -1718,7 +1718,7 @@ YY_BUFFER_STATE v29nfy__scan_bytes  (yyconst char * yybytes, int  _yybytes_len )
 	char *buf;
 	yy_size_t n;
 	int i;
-    
+
 	/* Get memory for full buffer, including space for trailing EOB's. */
 	n = _yybytes_len + 2;
 	buf = (char *) v29nfy_alloc(n  );
@@ -1748,7 +1748,7 @@ YY_BUFFER_STATE v29nfy__scan_bytes  (yyconst char * yybytes, int  _yybytes_len )
 
 static void yy_fatal_error (yyconst char* msg )
 {
-    	(void) fprintf( stderr, "%s\n", msg );
+	(void) fprintf( stderr, "%s\n", msg );
 	exit( YY_EXIT_FAILURE );
 }
 
@@ -1769,19 +1769,19 @@ static void yy_fatal_error (yyconst char* msg )
 		} \
 	while ( 0 )
 
-/* Accessor  methods (get/set functions) to struct members. */
+/* Accessors methods (get/set functions) to struct members. */
 
 /** Get the current line number.
- * 
+ *
  */
 int v29nfy_get_lineno  (void)
 {
-        
+
     return v29nfy_lineno;
 }
 
 /** Get the input stream.
- * 
+ *
  */
 FILE *v29nfy_get_in  (void)
 {
@@ -1789,7 +1789,7 @@ FILE *v29nfy_get_in  (void)
 }
 
 /** Get the output stream.
- * 
+ *
  */
 FILE *v29nfy_get_out  (void)
 {
@@ -1797,7 +1797,7 @@ FILE *v29nfy_get_out  (void)
 }
 
 /** Get the length of the current token.
- * 
+ *
  */
 int v29nfy_get_leng  (void)
 {
@@ -1805,7 +1805,7 @@ int v29nfy_get_leng  (void)
 }
 
 /** Get the current token.
- * 
+ *
  */
 
 char *v29nfy_get_text  (void)
@@ -1815,18 +1815,17 @@ char *v29nfy_get_text  (void)
 
 /** Set the current line number.
  * @param line_number
- * 
+ *
  */
 void v29nfy_set_lineno (int  line_number )
 {
-    
     v29nfy_lineno = line_number;
 }
 
 /** Set the input stream. This does not discard the current
  * input buffer.
  * @param in_str A readable stream.
- * 
+ *
  * @see v29nfy__switch_to_buffer
  */
 void v29nfy_set_in (FILE *  in_str )
@@ -1880,7 +1879,7 @@ static int yy_init_globals (void)
 /* v29nfy_lex_destroy is for both reentrant and non-reentrant scanners. */
 int v29nfy_lex_destroy  (void)
 {
-    
+
     /* Pop the buffer stack, destroying each element. */
 	while(YY_CURRENT_BUFFER){
 		v29nfy__delete_buffer(YY_CURRENT_BUFFER  );
