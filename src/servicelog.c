power-ras/libservicelog

commit ef2c19862b63ad7cffb737cbc6dfb73f78db06fa

src/servicelog.c: Replace sprintf() with secure variants()
As part of the security clean up, sprintf() are replaced
with more secure version snprintf(). In this case, simple
assignment make it less error prone and snprintf() calculates
remaining buf size for every snprintf().

Signed-off-by: Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>
Cc: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
[Fixed buf offset - Vasant]
Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>

src/servicelog.c
@@ -244,6 +244,7 @@ sl_printf(FILE *stream, char *fmt, ...)
 	int tmpbuf_len, width = 0, prnt_len;
 	char buf[1024], tmpbuf[1024];
 	char *newline = NULL, *brkpt = NULL;
+	int size = (*buf + sizeof(buf) - 1);
 
 	memset(tmpbuf, 0, sizeof(tmpbuf));
 	memset(buf, 0, sizeof(buf));
@@ -278,10 +279,12 @@ sl_printf(FILE *stream, char *fmt, ...)
 
 			if (newline != NULL) {
 				prnt_len = newline - &tmpbuf[offset] + 1;
-				snprintf(buf + buf_offset, prnt_len,
+				size -= buf_offset;
+				snprintf(buf + buf_offset, size,
 					 "%s", &tmpbuf[offset]);
 				buf_offset = strlen(buf);
-				buf_offset += sprintf(buf + buf_offset, "\n");
+				buf[*buf + buf_offset] = "\n";
+				buf_offset += 1;
 				offset += prnt_len;
 				line_offset = 0;
 				break;
@@ -297,15 +300,18 @@ sl_printf(FILE *stream, char *fmt, ...)
 			}
 
 			/* print up to the last brkpt */
-			snprintf(buf + buf_offset, prnt_len, "%s", &tmpbuf[offset]);
+			size -= buf_offset;
+			snprintf(buf + buf_offset, size, "%s", &tmpbuf[offset]);
 			buf_offset = strlen(buf);
-			buf_offset += sprintf(buf + buf_offset, "\n");
+			buf[*buf + buf_offset] = "\n";
+			buf_offset += 1;
 			offset += prnt_len;
 			line_offset = 0;
 		}
 	}
 
-	prnt_len = sprintf(buf + buf_offset, "%s", &tmpbuf[offset]);
+	size -= buf_offset;
+	prnt_len = snprintf(buf + buf_offset, size, "%s", &tmpbuf[offset]);
 	line_offset += prnt_len;
 
 	return fprintf(stream, "%s", buf);

commit 6e7c1421bfbfc7a8ef91526288b9f75b8bd28d6b

Fix logical issue in assignment
variable size is assigned a value(*buf + ...), where *buf contains garbage
value till this point and can result wrong size variable. buf[...] line seems
to place newline byte in last.

This patch fixes above mentioned issue.

Fixes: ef2c1986 (src/servicelog.c: Replace sprintf() with secure variants())
Signed-off-by: Ankit Kumar <ankit@linux.vnet.ibm.com>
Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>

src/servicelog.c
@@ -244,7 +244,7 @@ sl_printf(FILE *stream, char *fmt, ...)
 	int tmpbuf_len, width = 0, prnt_len;
 	char buf[1024], tmpbuf[1024];
 	char *newline = NULL, *brkpt = NULL;
-	int size = (*buf + sizeof(buf) - 1);
+	int size = sizeof(buf) - 1;
 
 	memset(tmpbuf, 0, sizeof(tmpbuf));
 	memset(buf, 0, sizeof(buf));
@@ -283,7 +283,7 @@ sl_printf(FILE *stream, char *fmt, ...)
 				snprintf(buf + buf_offset, size,
 					 "%s", &tmpbuf[offset]);
 				buf_offset = strlen(buf);
-				buf[*buf + buf_offset] = "\n";
+				buf[*buf + buf_offset] = '\n';
 				buf_offset += 1;
 				offset += prnt_len;
 				line_offset = 0;
@@ -303,7 +303,7 @@ sl_printf(FILE *stream, char *fmt, ...)
 			size -= buf_offset;
 			snprintf(buf + buf_offset, size, "%s", &tmpbuf[offset]);
 			buf_offset = strlen(buf);
-			buf[*buf + buf_offset] = "\n";
+			buf[*buf + buf_offset] = '\n';
 			buf_offset += 1;
 			offset += prnt_len;
 			line_offset = 0;