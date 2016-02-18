power-ras/libservicelog

commit baebe06b176ddaa007a24479d6bf1944a6ecad77

src/v29_compat.c: Replace strcpy() with secure variants
Replace all the occurrences of strcpy() with strncpy(). We
calculate the buffer size in some cases, before copying to
avoid over runs.

Signed-off-by: Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>
Cc: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>

src/v29_compat.c
@@ -407,7 +407,7 @@ convert_encl_to_v29(servicelog *log, struct sl_event *ev, void **data29,
 	next += sizeof(encl29);
 
 	if (description_sz) {
-		strcpy(next, ev->description);
+		strncpy(next, ev->description, (ev29_sz - sizeof(encl29) - 1));
 		ev29->description = next;
 		next += description_sz;
 	}
@@ -513,6 +513,7 @@ convert_v1_repair_to_v29(servicelog *log, struct sl_repair_action *rpr1,
 	char *next;
 	size_t ev29_sz;
 	struct sl_event *re, *repaired_events = NULL;
+	int str_size;
 
 	if (find_repaired_events(log, rpr1->id, &repaired_events) != 0)
 		repaired_events = NULL;
@@ -560,13 +561,15 @@ convert_v1_repair_to_v29(servicelog *log, struct sl_repair_action *rpr1,
 	next = (char*) ev29;
 	next += sizeof(rpr29);
 
+	str_size = (ev29_sz - sizeof(rpr29) - 1);
 	if (location_sz) {
-		strcpy(next, rpr1->location);
+		strncpy(next, rpr1->location, str_size);
 		ev29->location = next;
 		next += location_sz;
+		str_size -= (strlen(location_sz) + 1);
 	}
 	if (procedure_sz) {
-		strcpy(next, rpr1->procedure);
+		strncpy(next, rpr1->procedure, str_size);
 		ev29->procedure = next;
 		next += procedure_sz;
 	}
@@ -1084,7 +1087,7 @@ _convert_v1_sl_notify_to_v29(struct v29_servicelog *slog, struct v29_sl_notify *
 	v29->method = v1->method;	// These happen to match.
 	v29->command_length = size - sizeof(struct v29_sl_notify);
 	cmd = ((char *) v29) + sizeof(struct v29_sl_notify);
-	strcpy(cmd, v1->command);
+	strncpy(cmd, v1->command, v29->command_length);
 
 	return 0;
 }

commit b23b85af62248cfe21ba4ca3e0b6c2d762bd284c

Fix variable dereference issue
Present code use strlen operation on variable of type integer, which will cause
seg fault.
This patch fixes above mentioned issue.

Fixes: baebe06b (src/v29_compat.c: Replace strcpy() with secure variants)
Signed-off-by: Ankit Kumar <ankit@linux.vnet.ibm.com>
Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>

src/v29_compat.c
@@ -566,7 +566,7 @@ convert_v1_repair_to_v29(servicelog *log, struct sl_repair_action *rpr1,
 		strncpy(next, rpr1->location, str_size);
 		ev29->location = next;
 		next += location_sz;
-		str_size -= (strlen(location_sz) + 1);
+		str_size -= (location_sz + 1);
 	}
 	if (procedure_sz) {
 		strncpy(next, rpr1->procedure, str_size);