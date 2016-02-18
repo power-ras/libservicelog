power-ras/libservicelog

commit 7150152fd292a9e5ea731fe0cedde8ec86a456bf

src/v29_print.c: Convert all sprintf() to snprintf()
During security audit, one of the recommendation was convert
all sprintf() to snprintf(). This patch, converts all the
sprintf() to snprintf() along will calculating the remaining
buf size after every snprintf, incase of forming multi statement
queries.

Signed-off-by: Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>
Cc: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
[Fixed buffer offset issue - Vasant]
Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>

src/v29_print.c
@@ -115,6 +115,7 @@ sl_printf(FILE *stream, char *fmt, ...)
     int         prnt_len;
     char        *newline = NULL;
     char        *brkpt = NULL;
+    int         size = (*buf + sizeof(buf) - 1);
 
     memset(tmpbuf, 0, sizeof(tmpbuf));
     memset(buf, 0, sizeof(buf));
@@ -152,9 +153,10 @@ sl_printf(FILE *stream, char *fmt, ...)
 
             if (newline != NULL) {
                 prnt_len = newline - &tmpbuf[offset] + 1;
-                snprintf(buf + buf_offset, prnt_len, "%s", &tmpbuf[offset]);
+                size -= buf_offset;
+                snprintf(buf + buf_offset, size, "%s", &tmpbuf[offset]);
                 buf_offset = strlen(buf);
-                buf_offset += sprintf(buf + buf_offset, "\n");
+                buf_offset += snprintf(buf + buf_offset, size, "\n");
                 offset += prnt_len;
                 line_offset = 0;
                 break;
@@ -171,16 +173,18 @@ sl_printf(FILE *stream, char *fmt, ...)
             }
 
             /* print up to the last brkpt */
-            snprintf(buf + buf_offset, prnt_len, "%s", &tmpbuf[offset]);
+            size -= buf_offset;
+            snprintf(buf + buf_offset, size, "%s", &tmpbuf[offset]);
             buf_offset = strlen(buf);
-            buf_offset += sprintf(buf + buf_offset, "\n");
+            buf_offset += snprintf(buf + buf_offset, size, "\n");
             offset += prnt_len;
             line_offset = 0;
         }
 
     }
 
-    prnt_len = sprintf(buf + buf_offset, "%s", &tmpbuf[offset]);
+    size -= buf_offset;
+    prnt_len = snprintf(buf + buf_offset, size, "%s", &tmpbuf[offset]);
     line_offset += prnt_len;
 
     len = fprintf(stream, "%s", buf);

commit e3bbfc00948a9061adb5707ac4fd623310da1782

Fix variable assignment issue in v29_print
variable size is assigned a value(*buf + ...), where *buf contains garbage
value till this point and can result wrong size variable.
This patch fixes above mentioned issue.

Fixes: 7150152f (src/v29_print.c: Convert all sprintf() to snprintf())
Signed-off-by: Ankit Kumar <ankit@linux.vnet.ibm.com>
Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>

src/v29_print.c
@@ -115,7 +115,7 @@ sl_printf(FILE *stream, char *fmt, ...)
     int         prnt_len;
     char        *newline = NULL;
     char        *brkpt = NULL;
-    int         size = (*buf + sizeof(buf) - 1);
+    int		size = sizeof(buf) - 1;
 
     memset(tmpbuf, 0, sizeof(tmpbuf));
     memset(buf, 0, sizeof(buf));